7d6b6f6f968002bd2935d5194d1b1176617843f6
Mr.Z <zrd0921@qq.com>
2023-06-15 10:36:57 +0800

    chore: update `@deot/dev@2.x`

--- jest.config.mjs
+++
@@ -1,9 +0,0 @@
-import options from '@deot/dev-cli/config/jest.config.js';
-
-export default {
-	...options,
-	moduleNameMapper: {
-		'^@deot/http$': '<rootDir>/packages/index/src',
-		'^@deot/http-(.*?)$': '<rootDir>/packages/$1/src'
-	}
-};
